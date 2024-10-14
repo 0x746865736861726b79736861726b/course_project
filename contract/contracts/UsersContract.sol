4b5c6fbaa1fc0749b070466c8d4b7b930694b316
To Kirill <tio.kirill@gmail.com>
2024-10-15 01:04:08 +0300

    ipfs node added to docker-compose

--- contract/contracts/UsersContract.sol
+++ contract/contracts/UsersContract.sol
@@ -92,4 +92,31 @@ contract UsersContract {
 
         return (userAddresses, roles, creationDates, userIds); 
     }
+
+    function verifySignature(address _account, bytes32 _messageHash, bytes memory _signature) public view returns (bool) {
+        bytes32 ethSignedMessageHash = getEthSignedMessageHash(_messageHash);
+        address signer = recoverSigner(ethSignedMessageHash, _signature);
+        
+        return (signer == _account);
+    }
+
+    function getEthSignedMessageHash(bytes32 _messageHash) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash));
+    }
+
+    function recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature) public pure returns (address) {
+        require(_signature.length == 65, "Invalid signature length");
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        assembly {
+            r := mload(add(_signature, 32))
+            s := mload(add(_signature, 64))
+            v := byte(0, mload(add(_signature, 96)))
+        }
+
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
 }
